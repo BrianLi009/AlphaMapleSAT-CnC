1f22535c35380f4cf907f03b86adefa4e4c6f852
Brian Li <brianli.li@mail.utoronto.ca>
2024-12-02 00:22:26 -0500

    update solve.sh with all options

--- solve.sh
+++ solve.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
-while getopts "s" opt
-do
-	case $opt in
-        s) s="-s" ;;
-	esac
-done
-shift $((OPTIND-1))
-
 n=$1 #order
 f=$2 #instance file name
+solver=$3 #solver type
+t=$4 #timeout in seconds
+mode=$5 #solver mode (optional)
 
-[ "$1" = "-h" -o "$1" = "--help" -o "$#" -ne 2 ] && echo "
+[ "$1" = "-h" -o "$1" = "--help" -o "$#" -lt 4 ] && echo "
 Description:
     Script for solving and generating drat proof for instance
 
 Usage:
-    ./solve-verify.sh n f e
+    ./solve.sh n f solver t [mode]
 
-Options:
-    [-l]: generate learnt clauses
+Arguments:
     <n>: the order of the instance/number of vertices in the graph
     <f>: file name of the CNF instance to be solved
+    <solver>: -cadical or -maplesat
+    <t>: timeout in seconds
+    [mode]: -cas (optional)
 " && exit
 
-#./cadical-ks/build/cadical-ks $f $f.drat --order $n --unembeddable-check 17 --perm-out $f.perm --proofsize 7168 | tee $f.log
-timeout 10 ./maplesat-ks/simp/maplesat_static $f -exhaustive=$f.exhaust -order=$n -no-pre -minclause --timeout | tee $f.log
+# Select solver based on arguments
+if [ "$solver" = "-cadical" ]; then
+    if [ "$mode" = "-cas" ]; then
+        timeout $t ./cadical-ks/build/cadical-ks $f --order $n | tee $f.log
+    else
+        timeout $t ./cadical-ks/build/cadical-ks $f | tee $f.log
+    fi
+elif [ "$solver" = "-maplesat" ]; then
+    if [ "$mode" = "-cas" ]; then
+        timeout $t ./maplesat-ks/simp/maplesat_static $f -order=$n -no-pre -minclause | tee $f.log
+    else
+        timeout $t ./maplesat-ks/simp/maplesat_static $f -no-pre -minclause | tee $f.log
+    fi
+else
+    echo "Invalid solver option. Use -cadical or -maplesat"
+    exit 1
+fi
