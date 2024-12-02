fa90013035ae4da91121255a07d4d29ec0ae479b
Brian Li <brianli.li@mail.utoronto.ca>
2024-12-02 00:42:45 -0500

    update simplify by conflicts with 2 options

--- simplification/simplify-by-conflicts.sh
+++ simplification/simplify-by-conflicts.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 
-# Option parsing
-while getopts ":s" opt; do
-  case $opt in
-    s) s=true ;;
-    \?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
-  esac
-done
-
-shift $((OPTIND -1))
-
 # Ensure parameters are specified on the command-line
-if [ -z "$3" ]; then
-  echo "Need filename, order, and the number of conflicts for which to simplify"
-  exit
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 filename order conflicts [-cas]"
+    echo "Need filename, order, and the number of conflicts for which to simplify"
+    exit 1
 fi
 
 f=$1 # Filename
 o=$2 # Order
 m=$3 # Number of conflicts
-e=$((o*(o-1)/2)) # Number of edge variables
+mode=$4 # Optional -cas parameter
 
 # Create necessary directories
 mkdir -p log
 
 f_dir=$f
-f_base=$(basename "$f")
 
-# Simplify m seconds
+# Simplify for m conflicts
 echo "simplifying for $m conflicts"
 
-./cadical-ks/build/cadical-ks "$f_dir" --order $o -o "$f_dir".simp1 -e "$f_dir".ext -n -c $m | tee "$f_dir".simplog
+if [ "$mode" = "-cas" ]; then
+    ./cadical-ks/build/cadical-ks "$f_dir" --order $o -o "$f_dir".simp1 -e "$f_dir".ext -n -c $m | tee "$f_dir".simplog
+else
+    ./cadical-ks/build/cadical-ks "$f_dir" -o "$f_dir".simp1 -e "$f_dir".ext -n -c $m | tee "$f_dir".simplog
+fi
 
 # Output final simplified instance
 ./gen_cubes/concat-edge.sh $o "$f_dir".simp1 "$f_dir".ext > "$f_dir".simp
